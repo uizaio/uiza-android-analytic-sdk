4ad29b7a19b2982f26ac0d94a303a50927ceb0a0
Nam Nguyen <namdinh@uiza.io>
2020-06-01 16:58:52 +0700

    update parser info

--- uzapi/src/main/java/com/uiza/api/exts/UZApiExt.kt
+++ uzapi/src/main/java/com/uiza/api/exts/UZApiExt.kt
@@ -5,14 +5,24 @@ import com.uiza.api.models.UZLiveInfo
 import timber.log.Timber
 
 fun String.toLiveInfo(): UZLiveInfo? {
-    val index = this.indexOf("?cm=")
-    if (index > 0) {
+    val json = this.parserInfo()
+    json?.let {
         try {
-            val json = String(Base64.decode(this.substring(index + 4), Base64.DEFAULT))
-            return json.fromJson(UZLiveInfo::class.java)
+            return@toLiveInfo it.fromJson(UZLiveInfo::class.java)
         } catch (e: Exception) {
             Timber.e(e)
         }
+    }
+    return null
+}
+
+@Throws(java.lang.Exception::class)
+fun String.parserInfo(): String? {
+    val fromIndex = this.indexOf("?cm=")
+    if (fromIndex > 0) {
+        val toIndex = this.indexOf("&", fromIndex)
+        val cm = if (toIndex > 0) this.substring(fromIndex + 4, toIndex) else this.substring(fromIndex + 4)
+        return String(Base64.decode(cm, Base64.DEFAULT))
     }
     return null
 }
